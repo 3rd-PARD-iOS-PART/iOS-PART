774adc8e12ce238b7d92e3f78f6ca1cbf1c6e364
haram22 <kimhr9889@handong.ac.kr>
2024-03-25 14:37:14 +0900

    [실습자료] 2차 세미나 실습 자료

---
+++ TabBar/TabBar/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by 김하람 on 2023/09/29.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setTabBar()
+    }
+    func setTabBar() {
+        let vc1 = UINavigationController(rootViewController: FirstViewController())
+        let vc2 = UINavigationController(rootViewController: SecondViewController())
+        let vc3 = UINavigationController(rootViewController: ThirdViewController())
+        
+        self.viewControllers = [vc1, vc2, vc3]
+        self.tabBar.tintColor = .white
+        self.tabBar.backgroundColor = .gray
+        
+        guard let tabBarItems = self.tabBar.items else {return}
+        tabBarItems[0].image = UIImage(systemName: "face.smiling")
+        tabBarItems[1].image = UIImage(systemName: "moon.fill")
+        tabBarItems[2].image = UIImage(systemName: "pawprint.fill")
+        tabBarItems[0].title = "smile"
+        tabBarItems[1].title = "moon"
+        tabBarItems[2].title = "pawprint"
+    }
+
+
+}
+
